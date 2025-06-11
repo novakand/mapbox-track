1f1f3dbda014c96985feb0e817d7bd74e4a6d497
novakand <a79264799999@gmail.com>
2025-06-12 00:59:46 +0300

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -25,7 +25,7 @@ import {
 } from '@ngx-translate/core';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { LocalizationService } from './services/localization.service';
